f52b304ce6235213a9dffcf599e5022132e55b9c
dorp <dorp@cs.uni-bonn.de>
2019-03-27 13:37:26 +0100

    simple running version of CLI

--- extract.py
+++ extract.py
@@ -1,29 +1,50 @@
 #!/usr/bin/env python3
 
+import argparse
 import shutil
 import subprocess
+import sys
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-docker_container = 'fkiecad/fact_extractor'
-input_file = '/home/dorp/container'
-target_directory = '/tmp/target'
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='FACT extractor CLI')
+    parser.add_argument('-c', '--container', help='docker container', default='fkiecad/fact_extractor')
+    parser.add_argument('-o', '--output_directory', help='path to extracted files', default=None)
+    parser.add_argument('ARCHIVE', type=str, nargs=1, help='Archive for extraction')
+
+    return parser.parse_args()
 
 
-tmpdir = TemporaryDirectory()
-tmp = tmpdir.name
+def call_docker(input_file, container, target):
+    tmpdir = TemporaryDirectory()
+    tmp = tmpdir.name
 
-for subpath in ['files', 'reports', 'input']:
-    Path(tmp, subpath).mkdir()
+    for subpath in ['files', 'reports', 'input']:
+        Path(tmp, subpath).mkdir()
 
-shutil.copy(input_file, str(Path(tmp, 'input', Path(input_file).name)))
+    shutil.copy(input_file, str(Path(tmp, 'input', Path(input_file).name)))
 
-subprocess.run('docker run --rm -v {}:/tmp/extractor -v /dev:/dev --privileged {}'.format(tmp, docker_container), shell=True)
+    subprocess.run('docker run --rm -v {}:/tmp/extractor -v /dev:/dev --privileged {}'.format(tmp, container), shell=True)
 
-shutil.copytree(str(Path(tmp, 'files')), target_directory)
+    print('Moving files to {}'.format(target))
+    shutil.copytree(str(Path(tmp, 'files')), target)
 
-try:
-    tmpdir.cleanup()
-except PermissionError:
-    subprocess.run('sudo rm -rf {}'.format(tmpdir.name), shell=True)
-    tmpdir.cleanup()
+    try:
+        tmpdir.cleanup()
+    except PermissionError:
+        subprocess.run('sudo rm -rf {}'.format(tmpdir.name), shell=True)
+        tmpdir.cleanup()
+
+
+def main():
+    arguments = parse_arguments()
+
+    output_directory = arguments.output_directory if arguments.output_directory else str(Path(__file__).parent / 'extracted_files')
+
+    call_docker(arguments.ARCHIVE[0], arguments.container, output_directory)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
