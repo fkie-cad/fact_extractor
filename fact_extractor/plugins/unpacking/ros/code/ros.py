8241aead9b94bd2e528bc90d8f525916e6db44aa
dorp <dorp@cs.uni-bonn.de>
2019-06-12 10:56:39 +0200

    replaced ros_pack with own python ros unpacker

---
+++ fact_extractor/plugins/unpacking/ros/code/ros.py
@@ -0,0 +1,93 @@
+import struct
+from pathlib import Path
+from typing import List, NoReturn
+
+NAME = 'ROSFile'
+MIME_PATTERNS = ['firmware/ros']
+VERSION = '0.7'
+
+MAXIMUM_PART_NUMBER = 100
+HEADER_VERSIONS_AND_SIZES = {
+    b'1.01': 48,
+    b'2.00': 80
+}
+
+
+def infer_endianess_from_file_count(header: bytes) -> str:
+    return '<' if struct.unpack('<I', header[0x20:0x24])[0] < MAXIMUM_PART_NUMBER else '>'
+
+
+def infer_header_size_from_version(header: bytes) -> int:
+    for version, size in HEADER_VERSIONS_AND_SIZES.items():
+        if header[0x04:0x08] == version:
+            return size
+    raise ValueError('Unknown ros header version {}'.format(header[0x04:0x08]))
+
+
+def calculate_file_count(header: bytes, endianess: str) -> int:
+    return struct.unpack('{}I'.format(endianess), header[0x20:0x24])[0]
+
+
+def generate_part_information(header: bytes, endianess: str, number_of_files: int) -> List[dict]:
+    parts = list()
+    header_size = infer_header_size_from_version(header)
+
+    for index in range(number_of_files):
+        offset = header_size + index * 32
+        parts.append(
+            {
+                'index': index,
+                'file_name': header[offset:offset + 16].decode().strip('\x00'),
+                'offset': struct.unpack('{}i'.format(endianess), header[offset + 16: offset + 20])[0]
+            }
+        )
+    return parts
+
+
+def is_last_part(index: int, number_of_parts: int) -> bool:
+    return index == number_of_parts - 1
+
+
+def calculate_part_sizes(rosfile: Path, parts: List[dict]) -> NoReturn:
+    for part in parts:
+        if is_last_part(part['index'], len(parts)):
+            part['size'] = rosfile.stat().st_size - part['offset']
+        else:
+            part['size'] = parts[part['index'] + 1]['offset'] - part['offset']
+
+
+def store_parts_in_tmp_dir(file_path: str, parts: List[dict], tmp_dir: str) -> NoReturn:
+    with open(file_path, 'rb') as ros_fd:
+        for part in parts:
+            part_file = Path(tmp_dir, part['file_name'])
+            ros_fd.seek(part['offset'])
+            part_file.write_bytes(ros_fd.read(part['size']))
+
+
+def unpack_function(file_path: str, tmp_dir: str) -> dict:
+    with open(file_path, 'rb') as fd:
+        header = fd.read(512)
+
+    endianess = infer_endianess_from_file_count(header)
+    number_of_files = calculate_file_count(header, endianess)
+
+    try:
+        parts = generate_part_information(header, endianess, number_of_files)
+    except ValueError as value_error:
+        return {'error': str(value_error)}
+
+    parts.sort(key=lambda x: x['index'])
+    calculate_part_sizes(Path(file_path), parts)
+    store_parts_in_tmp_dir(file_path, parts, tmp_dir)
+
+    return {
+        'file_information': parts,
+        'endianess': endianess,
+        'ros_header_version': header[0x04:0x08].decode()
+    }
+
+
+# ----> Do not edit below this line <----
+def setup(unpack_tool):
+    for item in MIME_PATTERNS:
+        unpack_tool.register_plugin(item, (unpack_function, NAME, VERSION))
---
+++ fact_extractor/plugins/unpacking/ros/test/test_ros.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+
+import pytest
+
+from helperFunctions.file_system import get_test_data_dir
+from test.unit.unpacker.test_unpacker import TestUnpackerBase
+from ..code.ros import infer_header_size_from_version, infer_endianess_from_file_count, unpack_function
+
+TEST_DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
+
+class TestRosUnpacker(TestUnpackerBase):
+
+    def test_unpacker_selection_generic(self):
+        self.check_unpacker_selection('firmware/ros', 'ROSFile')
+
+    def test_extraction(self):
+        in_file = os.path.join(TEST_DATA_DIR, 'test.ros')
+        files, meta_data = self.unpacker.extract_files_from_file(in_file, self.tmp_dir.name)
+        self.assertEqual(len(files), 6, 'file number incorrect ({})'.format(files.__str__()))
+        self.assertIn('{}/DATETIME_C'.format(self.tmp_dir.name), files, 'Not all files found')
+        self.assertIn('file_information', meta_data, 'Output meta not set')
+
+
+def test_infer_endianess():
+    little = b'\x00' * 0x20 + b'\x05\x00\x00\x00'
+    big = b'\x00' * 0x20 + b'\x00\x00\x00\x05'
+    assert infer_endianess_from_file_count(little) == '<'
+    assert infer_endianess_from_file_count(big) == '>'
+
+
+def test_infer_header_size():
+    v1 = b'\x00' * 4 + b'1.01'
+    v2 = b'\x00' * 4 + b'2.00'
+    unknown = b'\x00' * 4 + b'1.02'
+
+    assert infer_header_size_from_version(v1) == 48
+    assert infer_header_size_from_version(v2) == 80
+
+    with pytest.raises(ValueError):
+        infer_header_size_from_version(unknown)
+
+
+def test_unpack_with_bad_file(tmpdir):
+    meta_data = unpack_function(str(Path(get_test_data_dir()) / 'generic_carver_test'), str(tmpdir))
+    assert 'error' in meta_data
+    assert 'Unknown ros header' in meta_data['error']
