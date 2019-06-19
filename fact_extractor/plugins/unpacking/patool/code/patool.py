cdb58a5c040953c7d3b8ff7788b20287bb2fe686
dorp <dorp@cs.uni-bonn.de>
2019-06-20 00:15:33 +0200

    moved arj to standalone plugin

--- fact_extractor/plugins/unpacking/patool/code/patool.py
+++ fact_extractor/plugins/unpacking/patool/code/patool.py
@@ -8,34 +8,17 @@ from tempfile import TemporaryDirectory
 from common_helper_process import execute_shell_command
 
 NAME = 'PaTool'
-MIME_PATTERNS = ['application/x-lrzip', 'application/x-cpio', 'application/x-archive', 'application/x-adf',
-                 'application/x-redhat-package-manager', 'application/x-rpm', 'application/x-lzop', 'application/x-lzh',
-                 'application/x-lha', 'application/x-cab', 'application/vnd.ms-cab-compressed', 'application/zpaq',
-                 'application/x-chm', 'application/x-arj', 'application/x-gzip',
-                 'application/gzip', 'application/x-bzip2', 'application/x-dms',
-                 'application/x-debian-package', 'application/x-rzip', 'application/x-tar', 'application/x-shar',
-                 'application/x-lzip', 'application/x-alzip', 'application/x-rar', 'application/rar',
-                 'application/java-archive',
-                 'application/x-iso9660-image', 'application/x-compress', 'application/x-arc', 'audio/flac',
-                 'application/x-ace', 'application/x-zoo', 'application/x-xz']
+MIME_PATTERNS = [
+    'application/x-lrzip', 'application/x-cpio', 'application/x-archive', 'application/x-adf',
+    'application/x-redhat-package-manager', 'application/x-rpm', 'application/x-lzop', 'application/x-lzh',
+    'application/x-lha', 'application/x-cab', 'application/vnd.ms-cab-compressed', 'application/zpaq',
+    'application/x-chm', 'application/x-gzip', 'application/gzip', 'application/x-bzip2', 'application/x-dms',
+    'application/x-debian-package', 'application/x-rzip', 'application/x-tar', 'application/x-shar',
+    'application/x-lzip', 'application/x-alzip', 'application/x-rar', 'application/rar',
+    'application/java-archive', 'application/x-iso9660-image', 'application/x-compress', 'application/x-arc',
+    'audio/flac','application/x-ace', 'application/x-zoo', 'application/x-xz'
+]
 VERSION = '0.5.1'
-
-
-def run_patool(file_path, tmp_dir):
-    return execute_shell_command('fakeroot patool extract --outdir {} {}'.format(tmp_dir, file_path), timeout=600)
-
-
-def handle_arj_files_explicitly(file_path, output, tmp_dir):
-    '''
-    arj needs target files to end on .arj
-    So here a fitting symlink is created
-    '''
-    if 'running /usr/bin/arj x -r' in output and not list(Path(tmp_dir).iterdir()):
-        with TemporaryDirectory() as staging_dir:
-            staged_path = str(Path(staging_dir) / '{}.arj'.format(Path(file_path).name))
-            symlink(file_path, staged_path)
-            output = run_patool(staged_path, tmp_dir)
-    return output
 
 
 def unpack_function(file_path, tmp_dir):
@@ -43,11 +26,9 @@ def unpack_function(file_path, tmp_dir):
     file_path specifies the input file.
     tmp_dir should be used to store the extracted files.
     """
-    output = run_patool(file_path, tmp_dir)
-
-    output = handle_arj_files_explicitly(file_path, output, tmp_dir)
-
-    return {'output': output}
+    return {
+        'output': execute_shell_command('fakeroot patool extract --outdir {} {}'.format(tmp_dir, file_path), timeout=600)
+    }
 
 
 # ----> Do not edit below this line <----
