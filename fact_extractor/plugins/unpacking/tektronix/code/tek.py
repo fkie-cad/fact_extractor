193deae7852493acfd4c239a6e89912723433efa
Christopher Krah <Christopher.Krah@fkie.fraunhofer.de>
2019-08-20 20:49:22 +0200

    refactoring tek plugin to be conform to flake8

--- fact_extractor/plugins/unpacking/tektronix/code/tek.py
+++ fact_extractor/plugins/unpacking/tektronix/code/tek.py
@@ -18,22 +18,18 @@ def unpack_function(file_path, tmp_dir):
     decoded = b''
     try:
         for rec in Path(file_path).read_text().splitlines():
-            #  addr = int(rec[1:5], 16)  # information not used by now
+            # _addr = int(rec[1:5], 16)     # Unused for now
             _dlen = int(rec[5:7], 16)
-            _crc1 = int(rec[7:9], 16)
             _data = rec[9:9 + _dlen * 2]
-            _crc2 = int(rec[-2:], 16)
 
-            expected_crc1 = sum(int(i, 16) for i in rec[1:7]) & 0xff
-            expected_crc2 = sum(int(i, 16) for i in _data) & 0xff
-
-            try:
-                decoded += binascii.unhexlify(_data)
-            except binascii.Error as tek_error:
-                return {'output': 'Unknown error in tek record decoding: {}'.format(str(tek_error))}
-
-            if _crc1 != expected_crc1 or _crc2 != expected_crc2:
+            if verify_crc(rec, _data):
                 return {'output': 'CRC mismatch in tek record: {}'.format(rec)}
+
+            _dec = decode_rec(_data, decoded)
+            if not isinstance(_dec, dict):
+                decoded += _dec
+            else:
+                return _dec
 
         Path(target_file).write_bytes(decoded)
 
@@ -43,6 +39,26 @@ def unpack_function(file_path, tmp_dir):
         return {'output': 'Failed to slice tek record: {}'.format(str(v_error))}
 
     return {'output': 'Successfully decoded tek file'}
+
+
+def decode_rec(_data, decoded):
+    try:
+        decoded += binascii.unhexlify(_data)
+    except binascii.Error as tek_error:
+        return {'output': 'Unknown error in tek record decoding: {}'.format(str(tek_error))}
+    return decoded
+
+
+def verify_crc(rec, data):
+    _crc1 = int(rec[7:9], 16)
+    _crc2 = int(rec[-2:], 16)
+
+    expected_crc1 = sum(int(i, 16) for i in rec[1:7]) & 0xff
+    expected_crc2 = sum(int(i, 16) for i in data) & 0xff
+
+    if _crc1 != expected_crc1 or _crc2 != expected_crc2:
+        return 1
+    return None
 
 
 # ----> Do not edit below this line <----
