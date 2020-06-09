ec3775199f2c5f642491f74684d0b712ba477d6d
Jean-Romain Garnier <github@jean-romain.com>
2020-06-09 16:03:29 +0200

    Update UEFI tools to python3

--- fact_extractor/plugins/unpacking/uefi/code/uefi.py
+++ fact_extractor/plugins/unpacking/uefi/code/uefi.py
@@ -19,7 +19,7 @@ def unpack_function(file_path, tmp_dir):
     tmp_dir should be used to store the extracted files.
     Optional: Return a dict with meta information
     '''
-    extraction_command = 'python2 {} --superbrute --extract --output {} {}'.format(TOOL_PATH, tmp_dir, file_path)
+    extraction_command = 'python3 {} --superbrute --extract --output {} {}'.format(TOOL_PATH, tmp_dir, file_path)
     output = execute_shell_command(extraction_command)
     return {'output': output}
 
