d4444c8467d483cec008c59bd7fed25ee09156cb
Jean-Romain Garnier <github@jean-romain.com>
2020-06-10 12:03:21 +0200

    Apply temporary patch for uefi-firmware-parser with python3

--- fact_extractor/plugins/unpacking/uefi/install.sh
+++ fact_extractor/plugins/unpacking/uefi/install.sh
@@ -11,6 +11,8 @@ cd ../../../install
 git clone https://github.com/theopolis/uefi-firmware-parser.git
 cd uefi-firmware-parser
 git checkout 4262dbbaab12c964242545e4f59a74c8f1b2f871 # known stable commit
+wget https://patch-diff.githubusercontent.com/raw/theopolis/uefi-firmware-parser/pull/83.patch # patch for python3 compatibilty
+git apply 83.patch
 sudo -E python3 setup.py install --force
 cp bin/uefi-firmware-parser ../../bin
 cd ..
