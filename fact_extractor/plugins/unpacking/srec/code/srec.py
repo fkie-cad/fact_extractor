f523880e6c0397dc49e38d509f6b330796dbc7f2
dorp <dorp@cs.uni-bonn.de>
2020-06-23 12:53:35 +0200

    fixed pep8 bug in srec

--- fact_extractor/plugins/unpacking/srec/code/srec.py
+++ fact_extractor/plugins/unpacking/srec/code/srec.py
@@ -19,8 +19,8 @@ def unpack_function(file_path, tmp_dir):
     decoded = b''
     try:
         srec = Path(file_path).read_text().splitlines()
-        for l in srec:
-            _, _, _, data = bincopy.unpack_srec(l)
+        for item in srec:
+            _, _, _, data = bincopy.unpack_srec(item)
             decoded += data
         Path(target_file).write_bytes(decoded)
 
