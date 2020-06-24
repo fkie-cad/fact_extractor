8708aa1917f9b44cc6ec57201f5e6e9ab120d015
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2020-06-24 10:53:35 +0200

    yaffs install restored and improved

---
+++ fact_extractor/plugins/unpacking/yaffs/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "------------------------------------"
+echo "          install unyaffs           "
+echo "------------------------------------"
+
+sudo apt-get install -y unyaffs || exit 1
+
+exit 0
