6d81b9fa683cca4cfbef44c67082eb362698c92c
any333 <anna-mariya.mateyna@gmx.de>
2020-12-08 22:12:48 +0100

    Made requested changes

--- fact_extractor/plugins/unpacking/generic_carver/code/generic_carver.py
+++ fact_extractor/plugins/unpacking/generic_carver/code/generic_carver.py
@@ -2,7 +2,6 @@
 This plugin unpacks all files via carving
 '''
 import logging
-import os
 import shutil
 from pathlib import Path
 import magic
@@ -25,7 +24,8 @@ def unpack_function(file_path, tmp_dir):
 
     drop_underscore_directory(tmp_dir)
     screening_meta = remove_false_positive_archives(file_path, tmp_dir)
-
+    print({'output': output, 'screening': screening_meta})
+    print('\n \n \n')
     return {'output': output, 'screening': screening_meta}
 
 
@@ -33,41 +33,37 @@ def remove_false_positive_archives(original_filename: str, unpack_directory: str
     binwalk_root = Path(unpack_directory) / f'_{original_filename}.extracted'
     if not binwalk_root.exists() or not binwalk_root.is_dir():
         return 'No files extracted, so nothing removed'
-    screening_log = ''
+    screening_logs = []
 
     for file_path in binwalk_root.iterdir():
         file_type = magic.from_file(str(file_path), mime=True)
 
         if 'zip' in file_type:
-            output_unzip = execute_shell_command('unzip -l {}'.format(file_path))
-            if 'not a zipfile' in output_unzip.replace('\n ', ''):
-                os.remove(file_path)
-                screening_log = 'file was not a zipfile and was removed'
-            else:
-                screening_log = 'file seems to be a valid archive'
+            screening_logs.append(check_archives_validity(file_path, 'unzip -l {}', 'not a zipfile'))
 
         elif 'x-tar' in file_type or 'gzip' in file_type or 'x-lzip' in file_type or 'x-bzip2' in file_type or 'x-xz' in file_type:
-            output_tar = execute_shell_command('tar -tvf {}'.format(file_path))
-            if 'does not look like a tar archive' in output_tar:
-                os.remove(file_path)
-                screening_log = 'file does not look like a valid archive and was removed'
-            else:
-                screening_log = 'file seems to be a valid archive'
+            screening_logs.append(check_archives_validity(file_path, 'tar -tvf {}', 'does not look like a tar archive'))
 
         # elif 'x-lrzip' in file_type or or 'rzip' in file_type or 'x-lz4' in file_type:
 
         elif 'x-7z-compressed' in file_type or 'x-compress' in file_type:
-            output_7z = execute_shell_command('7z l {}'.format(file_path))
-            if 'Is not archive' in output_7z or 'Can not open the file as [7z] archive' in output_7z:
-                os.remove(file_path)
-                screening_log = 'file is not a valid archive and was removed'
-            else:
-                screening_log = 'file seems to be a valid archive'
+            result_not_archive = check_archives_validity(file_path, '7z l {}', 'Is not archive')
+            if result_not_archive is not None:
+                screening_logs.append(result_not_archive)
 
-        else:
-            screening_log = 'archive type not supported yet'
+            result_not_7z = check_archives_validity(file_path, '7z l {}', 'Can not open the file as [7z] archive')
+            if result_not_7z is not None:
+                screening_logs.append(result_not_7z)
 
-    return screening_log
+    return screening_logs
+
+
+def check_archives_validity(file_path, command, search_string):
+    output = execute_shell_command(command.format(file_path))
+    if search_string in output.replace('\n ', ''):
+        file_path.unlink()
+        screening_log = '{} was removed'.format(str(file_path).rsplit('/', 1)[-1])
+        return screening_log
 
 
 def drop_underscore_directory(tmp_dir):
