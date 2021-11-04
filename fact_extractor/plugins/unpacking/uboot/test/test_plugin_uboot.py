4f93e79cab0ee5eefbe96a1110a5f816b3a91234
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2021-11-04 11:12:05 +0100

    added unpacking of device tree blobs to u-boot unpacker

--- fact_extractor/plugins/unpacking/uboot/test/test_plugin_uboot.py
+++ fact_extractor/plugins/unpacking/uboot/test/test_plugin_uboot.py
@@ -19,3 +19,14 @@ class TestUBootUnpacker(TestUnpackerBase):
 
         assert len(extracted_files) == 3, 'not all files extracted'
         assert any('uboot.lzma' in extracted_file for extracted_file in extracted_files), 'main container not extracted'
+
+    def test_extraction_dtb(self):
+        test_file_path = os.path.join(TEST_DATA_DIR, 'uboot.dtb')
+        extracted_files, meta_data = self.unpacker.extract_files_from_file(test_file_path, self.tmp_dir.name)
+
+        assert meta_data['plugin_used'] == 'Uboot', 'wrong plugin applied'
+
+        assert len(extracted_files) == 4, 'not all files extracted'
+        assert any(f.endswith('.dtb') for f in extracted_files), 'dtb not extracted'
+        assert 'extract-dtb' in meta_data
+        assert 'Dumped 01_dtbdump_Manufac_XYZ1234ABC.dtb' in meta_data['extract-dtb']
