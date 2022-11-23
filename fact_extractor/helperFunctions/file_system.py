148c60759cc905661f0d289aad8cb1198a15388d
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2022-11-23 14:07:56 +0100

    moved chown function to helper functions

--- fact_extractor/helperFunctions/file_system.py
+++ fact_extractor/helperFunctions/file_system.py
@@ -1,4 +1,8 @@
+import logging
 from pathlib import Path
+from re import match
+
+from common_helper_process import execute_shell_command_get_return_code
 
 SRC_DIR_PATH = Path(__file__).parent.parent.absolute()
 
@@ -40,3 +44,13 @@ def file_name_sanitize(file_path) -> str:
     Returns file path without directory traversal
     '''
     return file_path.replace('../', '')
+
+
+def change_owner_of_output_files(files_dir: Path, owner: str) -> int:
+    if not match(r'\d+:\d+', owner):
+        logging.error('ownership string should have the format <user id>:<group id>')
+        return 1
+
+    _, return_code_chown = execute_shell_command_get_return_code(f'sudo chown -R {owner} {files_dir}')
+    _, return_code_chmod = execute_shell_command_get_return_code(f'sudo chmod -R u+rw {files_dir}')
+    return return_code_chmod | return_code_chown
