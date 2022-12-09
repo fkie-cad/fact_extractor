a440959813b3d15334b1bfd6682afd58683e1368
LukasEpheser <mantraepheser@gmail.com>
2022-12-09 11:14:20 +0100

    extract kernel image and footer information

--- fact_extractor/plugins/unpacking/draytek_vigor_167/test/test_draytek_vigor_167.py
+++ fact_extractor/plugins/unpacking/draytek_vigor_167/test/test_draytek_vigor_167.py
@@ -12,17 +12,18 @@ class TestDraytekVigor167Unpacker(TestUnpackerBase):
         self.check_unpacker_selection('firmware/draytek-vigor-167', 'Draytek Vigor 167')
 
     def test_extraction(self):
-        input_file = Path(TEST_DATA_DIR, 'valid_draytekvigor167_image.bin')
-        unpacked_files, meta_data = self.unpacker.extract_files_from_file(input_file, self.tmp_dir.name)
+        test_file = Path(TEST_DATA_DIR, 'valid_draytekvigor167_image.bin')
+        unpacked_files, meta_data = self.unpacker.extract_files_from_file(test_file, self.tmp_dir.name)
 
         self.assertEqual(meta_data['output'], 'successfully unpacked image')
-        self.assertEqual(len(unpacked_files), 1, 'number of extracted files not correct')
-        self.assertEqual(unpacked_files[0], str(Path(self.tmp_dir.name) / 'squashfs_root'), 'file name not correct')
-        squashfs_binary = get_binary_from_file(unpacked_files[0])
+        self.assertEqual(len(unpacked_files), 2, 'number of extracted files not correct')
+        self.assertEqual(unpacked_files[0], str(Path(self.tmp_dir.name) / 'kernel_image'), 'file name not correct')
+        self.assertEqual(unpacked_files[1], str(Path(self.tmp_dir.name) / 'squashfs_root'), 'file name not correct')
+        squashfs_binary = get_binary_from_file(unpacked_files[1])
         squashfs_hash = get_sha256(squashfs_binary)
         self.assertEqual(squashfs_hash, '73b648f484ab0a34ce00729ce8b7ef183885b4b5c540344a8451d18fe94cc2fa')
 
-        input_file = Path(TEST_DATA_DIR, 'invalid_draytekvigor167_image_struct_error.bin')
-        unpacked_files, meta_data = self.unpacker.extract_files_from_file(input_file, self.tmp_dir.name)
+        test_file = Path(TEST_DATA_DIR, 'invalid_draytekvigor167_image_struct_error.bin')
+        unpacked_files, meta_data = self.unpacker.extract_files_from_file(test_file, self.tmp_dir.name)
 
-        self.assertIn('failed to extract header', meta_data['output'])
+        self.assertIn('failed to recognize firmware container', meta_data['output'])
