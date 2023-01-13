8fa7b7aae544ca31873078eae202dc36c41d088f
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2023-01-13 10:53:09 +0100

    pep8/black fix

--- fact_extractor/plugins/unpacking/generic_carver/test/test_plugin_generic_carver_binwalk.py
+++ fact_extractor/plugins/unpacking/generic_carver/test/test_plugin_generic_carver_binwalk.py
@@ -15,13 +15,14 @@ TEST_DATA_DIR = Path(__file__).parent / 'data'
 
 
 class TestGenericCarver(TestUnpackerBase):
-
     def test_unpacker_selection_generic(self):
         self.check_unpacker_selection('generic/carver', 'generic_carver')
 
     def test_extraction(self):
         in_file = f'{get_test_data_dir()}/generic_carver_test'
-        files, meta_data = self.unpacker._extract_files_from_file_using_specific_unpacker(in_file, self.tmp_dir.name, self.unpacker.unpacker_plugins['generic/carver'])
+        files, meta_data = self.unpacker._extract_files_from_file_using_specific_unpacker(
+            in_file, self.tmp_dir.name, self.unpacker.unpacker_plugins['generic/carver']
+        )
         files = set(files)
         assert len(files) == 1, 'file number incorrect'
         assert files == {f'{self.tmp_dir.name}/64.zip'}, 'not all files found'
@@ -30,7 +31,9 @@ class TestGenericCarver(TestUnpackerBase):
 
     def test_extraction_of_filtered_files(self):
         in_file = str(TEST_DATA_DIR / 'fake_xz.bin')
-        files, meta_data = self.unpacker._extract_files_from_file_using_specific_unpacker(in_file, self.tmp_dir.name, self.unpacker.unpacker_plugins['generic/carver'])
+        files, meta_data = self.unpacker._extract_files_from_file_using_specific_unpacker(
+            in_file, self.tmp_dir.name, self.unpacker.unpacker_plugins['generic/carver']
+        )
         assert len(files) == 0
         assert 'was removed' in meta_data['filter_log']
 
@@ -50,7 +53,6 @@ def filter_test_setup(filename) -> FilterTest:
         shutil.copyfile(source_file, test_file)
         arch_filter = ArchivesFilter(temp_dir)
         yield FilterTest(test_file, source_file, arch_filter)
-
 
 
 @pytest.mark.parametrize('filename', ['fake_zip.zip', 'fake_tar.tar', 'fake_7z.7z', 'fake_xz.xz', 'fake_gz.gz'])
