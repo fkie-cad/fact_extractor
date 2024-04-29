ef3f5d2be1069e0152beac48d6359bf44d64e126
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2024-04-29 11:22:19 +0200

    updated 7z installation to v24.09

--- fact_extractor/plugins/unpacking/sevenz/install.sh
+++ fact_extractor/plugins/unpacking/sevenz/install.sh
@@ -4,28 +4,29 @@ set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 echo "------------------------------------"
-echo "      install p7z from source       "
+echo "             install 7z             "
 echo "------------------------------------"
 
 
-# install newest version of p7zip
-sudo apt-get remove -y p7zip-full
-
+# install newest version of 7z
 mkdir -p /tmp/fact_build
 cd /tmp/fact_build
 
-wget -O 7zip.tar.bz2 https://sourceforge.net/projects/p7zip/files/latest/download
-# remove possible artifacts from previous installation (: == NOP)
-rm -rf ./p7zip* || :
-tar xvjf 7zip.tar.bz2
-cd p7zip*
-# gcc >= 11 has -Wnarrowing as default flag which leads to an error during compilation
-# g++ will try to use standard C++17 but the code is not compatible -> use C++14
-sed -i 's/CXXFLAGS=-c -I. \\/CXXFLAGS=-c -I. -Wno-narrowing -std=c++14 \\/g' makefile.glb  || echo "Warning: Could not apply makefile patch"
-cp makefile.linux_any_cpu makefile.machine
-make -j"$(nproc)" all3
-sudo ./install.sh
-cd ..
-rm -fr p7zip* 7zip.tar.bz2
+VERSION="2409"
+ARCH=$(uname -m)
+if [ "${ARCH}" = "x86_64" ]; then
+  ARCH_SUFFIX="x64"
+elif [ "${ARCH}" = "aarch64" ]; then
+  ARCH_SUFFIX="arm64"
+else
+  echo "Error: Unsupported architecture ${ARCH}"
+  exit 1
+fi
+
+FILE="7z${VERSION}-linux-${ARCH_SUFFIX}.tar.xz"
+wget "https://www.7-zip.org/a/${FILE}"
+tar xvf "${FILE}" 7zzs
+sudo mv 7zzs /usr/local/bin/
+rm "${FILE}"
 
 exit 0
