c4d11f9c214ebd1c9b4d6cd15d1a7acefef7230e
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2025-03-13 19:39:52 +0100

    feat: added support for big endian kernel images

--- fact_extractor/plugins/unpacking/linuxkernel/internal/extractor.py
+++ fact_extractor/plugins/unpacking/linuxkernel/internal/extractor.py
@@ -1,25 +1,35 @@
-import os
+from pathlib import Path
+from typing import Iterable
 
-from pattern_searcher import PatternSearcher
+from plugins.unpacking.linuxkernel.internal.pattern_searcher import PatternSearcher
 
 
 class Extractor:
-    def __init__(self, file_path, output_dir):
-        self.file_path = file_path
-        self.output_dir = output_dir
-        with open(file_path, 'rb') as f:
+    def __init__(self, file_path: str, output_dir: str):
+        self.file_path = Path(file_path)
+        self.output_dir = Path(output_dir)
+        with self.file_path.open('rb') as f:
             self.raw_data = f.read()
         self.ps = PatternSearcher(self.raw_data)
         self.offsets = self.ps.find_all_headers()
 
-    def _find_offsets(self):
-        return self.offsets
+    @staticmethod
+    def _get_reversed_chunks(data: bytes, offset: int, chunk_size: int) -> Iterable[bytes]:
+        # converts endianness of data and returns it as chunks
+        for i in range(offset, len(data), chunk_size):
+            yield data[i : i + chunk_size][::-1]
 
     def extracted_files(self):
         for algo in self.offsets:
             for offset in self.offsets[algo]:
-                fn = 'vmlinux_{}_{}.{}'.format(algo, offset, self.ps.algorithms[algo]['suffix'])
-                file_path = os.path.join(self.output_dir, fn)
-                with open(file_path, 'wb') as f:
-                    f.write(self.raw_data[offset:])
+                filename = f'vmlinux_{algo}_{offset}.{self.ps.algorithms[algo]["suffix"]}'
+                file_path = Path(self.output_dir) / filename
+                if algo.endswith('BE32'):
+                    data = b''.join(self._get_reversed_chunks(self.raw_data, offset, 32 // 8))
+                    magic_offset = self.ps.algorithms[algo].get('magic_offset', 0)
+                    if magic_offset > 0:
+                        data = data[magic_offset:]
+                else:
+                    data = self.raw_data[offset:]
+                file_path.write_bytes(data)
                 yield {'file_path': file_path, 'command': self.ps.algorithms[algo]['command']}
