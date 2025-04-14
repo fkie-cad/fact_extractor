9b769b942fbe132d16621768f4559607a20d7cc6
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2025-04-14 14:28:09 +0200

    feat: added new unpacking plugin base class

--- fact_extractor/plugins/unpacking/generic_carver/test/test_plugin_generic_carver.py
+++ fact_extractor/plugins/unpacking/generic_carver/test/test_plugin_generic_carver.py
@@ -1,4 +1,3 @@
-import re
 import zlib
 from pathlib import Path
 
@@ -18,7 +17,7 @@ class TestGenericCarver(TestUnpackerBase):
     def test_extraction(self):
         in_file = f'{get_test_data_dir()}/generic_carver_test'
         files, meta_data = self.unpacker._extract_files_from_file_using_specific_unpacker(
-            in_file, self.tmp_dir.name, self.unpacker.unpacker_plugins['generic/carver']
+            in_file, self.tmp_dir.name, self.unpacker.unpacking_plugins['generic_carver']
         )
         files = set(files)
         assert len(files) == 3, 'file number incorrect'
@@ -28,7 +27,7 @@ class TestGenericCarver(TestUnpackerBase):
     def test_zlib_carving(self):
         in_file = TEST_DATA_DIR / 'zlib_carving_test'
         files, meta_data = self.unpacker._extract_files_from_file_using_specific_unpacker(
-            in_file, self.tmp_dir.name, self.unpacker.unpacker_plugins['generic/carver']
+            in_file, self.tmp_dir.name, self.unpacker.unpacking_plugins['generic_carver']
         )
         assert len(files) == 9, 'file number incorrect'
         assert sum(1 if f.endswith('.zlib_carver') else 0 for f in files), 'wrong number of carved zlib streams'
@@ -40,7 +39,7 @@ class TestGenericCarver(TestUnpackerBase):
         in_file = TEST_DATA_DIR / 'carving_test_file'
         assert Path(in_file).is_file()
         files, meta_data = self.unpacker._extract_files_from_file_using_specific_unpacker(
-            str(in_file), self.tmp_dir.name, self.unpacker.unpacker_plugins['generic/carver']
+            str(in_file), self.tmp_dir.name, self.unpacker.unpacking_plugins['generic_carver']
         )
         files = set(files)
         assert len(files) == 4, 'file number incorrect'
@@ -53,12 +52,11 @@ class TestGenericCarver(TestUnpackerBase):
         in_file = TEST_DATA_DIR / f'fake_{file_format}.{file_format}'
         assert Path(in_file).is_file()
         meta = unpack_function(str(in_file), self.tmp_dir.name)
-        assert meta == {'output': 'No valid chunks found.'}
+        assert 'No valid chunks found.' in meta['output']
 
     @pytest.mark.parametrize(('file_format', 'expected_size'), [('bz2', 52), ('zip', 170)])
     def test_trailing_data(self, file_format, expected_size):
         in_file = TEST_DATA_DIR / f'trailing_data.{file_format}'
         assert Path(in_file).is_file()
         meta = unpack_function(str(in_file), self.tmp_dir.name)
-        carved_size = int(re.search(r'size: (\d+)', meta['output']).group(1))
-        assert carved_size == expected_size
+        assert f'chunk: 0x0-{hex(expected_size)}' in meta['output']
