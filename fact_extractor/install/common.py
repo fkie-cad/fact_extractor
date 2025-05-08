18dd26e6c01fb0857a81dfacd1461d1ac174fe1d
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2025-05-08 17:56:31 +0200

    feat: added buffalo bgn image unpacker

--- fact_extractor/install/common.py
+++ fact_extractor/install/common.py
@@ -27,7 +27,7 @@ def install_apt_dependencies():
     apt_install_packages(*APT_DEPENDENCIES)
 
 
-def _install_magic(version='v0.2.6'):
+def _install_magic(version='v0.2.7'):
     with OperateInDirectory(BIN_DIR):
         sp.run(
             [
