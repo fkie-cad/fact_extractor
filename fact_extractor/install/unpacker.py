f23865e197c4e2de5c04fb055e78b7768881c29d
Lance Ware <lance.ware@finitestate.io>
2025-06-04 12:49:28 -0400

    * fix

--- fact_extractor/install/unpacker.py
+++ fact_extractor/install/unpacker.py
@@ -161,7 +161,7 @@ DEPENDENCIES = {
             'git+https://github.com/armbues/python-entropy',  # To be checked. Original dependency was deleted.
             'git+https://github.com/fkie-cad/common_helper_unpacking_classifier.git',
             'git+https://github.com/fkie-cad/fact_helper_file.git',
-            'git+https://github.com/wummel/patool.git@4.0.0',
+            'git+https://github.com/wummel/patool@4.0.0',
             'archmage',
             # jefferson + deps
             'git+https://github.com/sviehb/jefferson.git',
