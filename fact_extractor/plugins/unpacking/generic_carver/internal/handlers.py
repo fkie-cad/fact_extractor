9bc9b528e5073e298b9309ccb2a665972d793787
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2025-06-26 08:39:59 +0200

    feat: add PNG carver to remove carved zlib streams from PNG files (#200)

--- fact_extractor/plugins/unpacking/generic_carver/internal/handlers.py
+++ fact_extractor/plugins/unpacking/generic_carver/internal/handlers.py
@@ -36,4 +36,27 @@ class Gm8126ImageHandler(Handler):
         )
 
 
-CUSTOM_HANDLERS = [ZlibCarvingHandler, Gm8126ImageHandler]
+class PngImageHandler(Handler):
+    NAME = 'png'
+    PATTERNS = (HexString('89 50 4e 47 0d 0a 1a 0a'),)
+
+    def calculate_chunk(self, file: File, start_offset: int) -> ValidChunk | None:
+        size = file.size()
+        # there is no size field in the header, but the file consists of chunks with their own headers which have a
+        # size field, so we can follow the chunks until we get to the end of the file
+        current_offset = start_offset + 8  # the first chunk starts
+        while True:
+            file.seek(current_offset)
+            chunk_size = struct.unpack('>I', file.read(4))[0]
+            chunk_type = file.read(4)
+            current_offset += 8 + chunk_size + 4  # 4*2 bytes header + chunk size + 4 bytes CRC
+            if chunk_type == b'IEND':
+                return ValidChunk(
+                    start_offset=start_offset,
+                    end_offset=current_offset,
+                )
+            if current_offset > size:
+                return None
+
+
+CUSTOM_HANDLERS = [ZlibCarvingHandler, Gm8126ImageHandler, PngImageHandler]
