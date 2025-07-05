c067dc8225ce13c35249b1cb3335640161691c0f
Andrew LaMarche <andrewjlamarche@gmail.com>
2025-07-05 23:23:20 +0000

    magic: bump to 0.2.8

--- fact_extractor/install/common.py
+++ fact_extractor/install/common.py
@@ -27,7 +27,7 @@ def install_apt_dependencies():
     apt_install_packages(*APT_DEPENDENCIES)
 
 
-def _install_magic(version='v0.2.7'):
+def _install_magic(version='v0.2.8'):
     with OperateInDirectory(BIN_DIR):
         sp.run(
             [
