e7facd4f5a7abc637813c0df9003dd5805a994da
Snowy Connection <79218470+snowy-connection@users.noreply.github.com>
2026-03-29 18:46:20 +0200

    upgrade draytek_arsenal to include the newest versions of dependencies

--- fact_extractor/plugins/unpacking/draytek_arsenal_wrapper/install.sh
+++ fact_extractor/plugins/unpacking/draytek_arsenal_wrapper/install.sh
@@ -7,6 +7,6 @@ echo "------------------------------------"
 echo "         install draytex_arsenal    "
 echo "------------------------------------"
 
-pip install git+https://github.com/snowy-connection/draytek-arsenal@216729aefdcb1073d56d9939078ad74c866be8be#subdirectory=draytek_arsenal
+pip install git+https://github.com/snowy-connection/draytek-arsenal@ef50af366d75a3ae4935a28f489be1b2a6db50a7#subdirectory=draytek_arsenal
 
 exit 0
