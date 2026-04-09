97b336b0a0cf12940f624d2989a08a699a9f547e
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2026-04-09 13:17:43 +0200

    fix: FIT unpacker fixes
    * fixed the problem that the unpacker did not extract data from nested nodes (depth >2)
    
    * fixed the issue that the unpacker stops after the first node that does not contain a 'data' entry (even though there could be more subnodes on the same level with such entries)

--- fact_extractor/plugins/unpacking/uboot/test/test_plugin_fit.py
+++ fact_extractor/plugins/unpacking/uboot/test/test_plugin_fit.py
@@ -11,13 +11,31 @@ class TestFITUnpacker(TestUnpackerBase):
     def test_unpacker_selection_generic(self):
         self.check_unpacker_selection('linux/device-tree', 'FIT')
 
-    def test_extraction(self):
+    def test_extraction_itb(self):
         test_file_path = Path(TEST_DATA_DIR) / 'fit.itb'
         extracted_files, meta_data = self.unpacker.extract_files_from_file(str(test_file_path), self.tmp_dir.name)
 
         assert meta_data['plugin_used'] == 'FIT', 'wrong plugin applied'
+        assert meta_data.get('error') is None
+        assert meta_data.get('output') is not None
 
         assert len(extracted_files) == EXPECTED_FILE_COUNT, 'not all files extracted'
-        assert all(
-            Path(element).name in ['kernel', 'fdt', 'rootfs'] for element in extracted_files
-        ), 'not all files extracted'
+        for element in extracted_files:
+            assert Path(element).name in ['kernel.bin', 'fdt.bin', 'rootfs.bin']
+
+    def test_extraction_dtb(self):
+        test_file_path = Path(TEST_DATA_DIR) / 'test.dtb'
+        extracted_files, meta_data = self.unpacker.extract_files_from_file(str(test_file_path), self.tmp_dir.name)
+
+        assert meta_data['plugin_used'] == 'FIT', 'wrong plugin applied'
+        assert meta_data.get('error') is None
+        assert meta_data.get('output') is not None
+
+        files = {p.name: p for f in extracted_files if (p := Path(f))}
+        assert 'kernel.bin' in files
+        assert files['kernel.bin'].read_bytes() == b'Hello'
+        assert 'ramdisk.bin' in files
+        assert files['ramdisk.bin'].read_bytes() == b'World', 'decompression failed'
+        assert 'nested.bin' in files
+        assert files['nested.bin'].read_bytes() == b'Nested', 'nested file not extracted'
+        assert 'unpacked data from node images/ramdisk/nested (6 bytes)' in meta_data['output']
